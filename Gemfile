35184e6be0041a962aea14f9f5a41b85a4774a4b
Edgars Jekabsons <edgarsj@Edgarss-MacBook-Pro.local>
2011-09-03 17:52:24 +0300

    Started new rails app 3.1

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+gem 'json'
+gem 'jquery-rails'
+gem 'capistrano'
+gem 'mongrel'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :development do 
+  gem 'rspec-rails'
+  gem 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TenderScraper::Application.config.secret_token = '4ea4a2c1802da554df2f1b8a6c51e47c3f5ab7ccf6c094b24de5f801b577070e241d5a8fbd68241919e6cee4182ab930a3bdcc17101c4a63deeac2ff7ddd3041'
